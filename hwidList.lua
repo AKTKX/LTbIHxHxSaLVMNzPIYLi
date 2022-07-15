9cfaf444654ba7453c83cff8ab10e2962fd39b67
AKTKX <109387924+AKTKX@users.noreply.github.com>
2022-07-15 22:11:05 +0200

    Update hwidList.lua

--- hwidList.lua
+++ hwidList.lua
@@ -1,5 +1,5 @@
 local hwidList = {
-    --'C4A2B623-5BD1-4E02-A9D8-383DCC9ABAF0'; --Gigant#0001
+    'C4A2B623-5BD1-4E02-A9D8-383DCC9ABAF0'; --Gigant#0001
     '38A83860-DCFB-4C60-90FA-5D68FC3435B3'; --cz.pa#0100
     --''; --
     --''; --
