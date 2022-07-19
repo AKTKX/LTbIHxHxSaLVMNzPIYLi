3d2a62f48f18b0e8bf6b31d2b9250a09981e2820
AKTKX <109387924+AKTKX@users.noreply.github.com>
2022-07-19 15:35:31 +0200

    Update hwidList.lua

--- hwidList.lua
+++ hwidList.lua
@@ -1,6 +1,6 @@
 local hwidList = {
     'C4A2B623-5BD1-4E02-A9D8-383DCC9ABAF0'; --Gigant#0001
-    --''; --
+    '23AA970F-3269-4C37-9B4F-98DBF34937BC'; --MrPatryk
     --''; --
     --''; --
     --''; --
